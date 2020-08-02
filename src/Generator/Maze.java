ffa27607ad406d07a55d022c5a17703e21d2632a
Uygur Tepe <uygur.t10@gmail.com>
2020-08-02 15:13:44 -0400

    added Switch cases for solvers

--- src/Generator/Maze.java
+++ src/Generator/Maze.java
@@ -1,15 +1,38 @@
 package Generator;
 
 import Solver.*;
+import java.util.Scanner;
 
 public class Maze {
     public static void main(String[] args){
-        MazeGenerator generator = new MazeGenerator(); // initialize and generate the maze using recursive backtracking
-        DijkstraSolve solver = new DijkstraSolve(generator.maze);
-        MazePanel.BuildGUI(solver.maze); // build the gui for displaying
-        // MazePanel.BuildGUI(generator.maze); // build the gui for displaying
-
-        RecursiveSolver solved = new RecursiveSolver(generator.maze);
-        MazePanel.BuildGUI(solved.maze);
+        MazeGenerator generated = new MazeGenerator(); // initialize and generate the maze using recursive backtracking
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Maze Generator & Solver Menu");
+        System.out.println("-----------------------------");
+        boolean flag = true;
+        while(flag){
+                System.out.println("[1] Display Generated Maze");
+                System.out.println("[2] Solve Maze using Modified Dijkstra's Algorithm");
+                System.out.println("[3] Solve Maze using a Recursive Solver");
+                System.out.println("[4] Exit");
+                int key = sc.nextInt();
+                switch (key) {
+                        case 1:
+                                MazePanel.BuildGUI(generated.maze); // build the gui for displaying
+                                break;
+                        case 2:
+                                DijkstraSolve solver = new DijkstraSolve(generated.maze);
+                                MazePanel.BuildGUI(solver.maze); // build the gui for displaying
+                                break;
+                        case 3: 
+                                RecursiveSolver solved = new RecursiveSolver(generated.maze);
+                                MazePanel.BuildGUI(solved.maze);
+                                break;
+                        default:
+                                flag = false;
+                                break;
+                }
+        }
+        sc.close();
     }
 }
